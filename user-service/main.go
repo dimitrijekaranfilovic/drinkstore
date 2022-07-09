4df0c6883161cfb09d30e01011cfc51753d6c569
Dimitrije Karanfilovic <dimitrije.karanfilovic@gmail.com>
2022-07-09 15:28:56 +0200

    Refactoring.

--- user-service/main.go
+++ user-service/main.go
@@ -25,9 +25,9 @@ func main() {
 	//admin
 	router.HandleFunc("/api/users/ban/{username}", requestHandler.BanUser).Methods("GET")
 	//samo ostali servisi
-	router.HandleFunc("/api/users/authorize-admin", requestHandler.AuthorizeAdmin).Methods("GET", "POST", "PUT", "PATCH")
+	router.HandleFunc("/api/users/authorize-admin", requestHandler.AuthorizeAdmin).Methods("GET")
 	//samo ostali servisi
-	router.HandleFunc("/api/users/authorize-user", requestHandler.AuthorizeUser).Methods("GET", "POST", "PUT", "PATCH")
+	router.HandleFunc("/api/users/authorize-user", requestHandler.AuthorizeUser).Methods("GET")
 	//samo ostali servisi
 	router.HandleFunc("/api/users/userId", requestHandler.GetUserIdFromJWT).Methods("GET")
 
