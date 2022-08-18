1b19b49afb9225fb9125b89d8e9b412320008ada
Dimitrije Karanfilovic <dimitrije.karanfilovic@gmail.com>
2022-08-18 15:34:08 +0200

    Implemented docker container creation for nginx. Refactor.

--- scripts/nginx-configuration.sh
+++ scripts/nginx-configuration.sh
@@ -5,8 +5,9 @@ if [ "" = "$PKG_OK" ]; then
   echo "$REQUIRED_PKG not installed. Install it first, then proceed with setup."
 else
     echo "Copying conf files..."
-    sudo cp -f nginx-setup/api_json_errors.conf /etc/nginx
-    sudo cp -rf nginx-setup/drinkstore_conf.d /etc/nginx
+    sudo cp -f ../nginx-setup-local/nginx.conf /etc/nginx
+    sudo cp -f ../nginx-setup-local/api_json_errors.conf /etc/nginx
+    sudo cp -rf ../nginx-setup-local/drinkstore_conf.d /etc/nginx
 
     echo "Removing logs..."
     sudo rm -rf /var/log/nginx/user_service /var/log/nginx/drink_service /var/log/nginx/comment_service /var/log/nginx/purchase_service
@@ -19,5 +20,4 @@ else
     sudo mkdir /var/log/nginx/comment_service/access /var/log/nginx/comment_service/error
     sudo mkdir /var/log/nginx/purchase_service/access /var/log/nginx/purchase_service/error
 
-    echo "Add this to the end of the http block in /etc/nginx/nginx.conf: include /etc/nginx/drinkstore_conf.d/drinkstore_api_gateway.conf;"
 fi
---
+++ scripts/run.sh
@@ -0,0 +1,39 @@
+
+
+while getopts s: flag
+do
+    case "${flag}" in
+        s) start=${OPTARG};;
+    esac
+done
+
+if [[ "$start" == "local" ]];then 
+    echo "Starting system localy..."
+
+    sudo systemctl start mongod
+    sudo service nginx start
+
+    gnome-terminal --tab --working-directory=/home/dimitrije/Desktop/drinkstore/frontend -- npm run serve-proxy
+    gnome-terminal --tab --working-directory=/home/dimitrije/Desktop/drinkstore/user-service -- go run main.go
+    gnome-terminal --tab --working-directory=/home/dimitrije/Desktop/drinkstore/drink-service -- go run main.go
+    gnome-terminal --tab --working-directory=/home/dimitrije/Desktop/drinkstore/comment-service -- go run main.go
+    gnome-terminal --tab --working-directory=/home/dimitrije/Desktop/drinkstore/purchase-service -- cargo run
+
+
+else 
+    echo "Starting system with docker containers..."
+    gnome-terminal --tab  -- sudo docker start -a user-service-database
+    gnome-terminal --tab  -- sudo docker start -a drink-service-database
+    gnome-terminal --tab  -- sudo docker start -a comment-service-database
+    gnome-terminal --tab  -- sudo docker start -a purchase-service-database
+    
+    gnome-terminal --tab  -- sudo docker start -a user-service
+    gnome-terminal --tab  -- sudo docker start -a drink-service
+    gnome-terminal --tab  -- sudo docker start -a comment-service
+    gnome-terminal --tab  -- sudo docker start -a purchase-service
+
+    gnome-terminal --tab  -- sudo docker start -a frontend
+    gnome-terminal --tab  -- sudo docker start -a api-gateway
+
+fi
+
